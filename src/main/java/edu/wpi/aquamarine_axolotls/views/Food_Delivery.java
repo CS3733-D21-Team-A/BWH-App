0cfd2a977e079b6db03adffe8e0301921a48adbe
evelyntrvn <evelyntran235@gmail.com>
2021-04-05 21:19:46 -0400

    Added images and updated food delivery

--- src/main/java/edu/wpi/aquamarine_axolotls/views/Food_Delivery.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/Food_Delivery.java
@@ -1,13 +1,42 @@
 package edu.wpi.aquamarine_axolotls.views;
 
 import edu.wpi.aquamarine_axolotls.Aapp;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class Food_Delivery extends Service_Request {
+
+    ObservableList<String> foodOptionList = FXCollections
+            .observableArrayList("Mac and Cheese", "Salad", "Pizza");
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField deliveryTime;
+
+    @FXML
+    private TextField roomNumber;
+
+    @FXML
+    private ComboBox foodOptions;
+
+    /*
+    @FXML
+    public void initialize(){
+        foodOptions.setItems(foodOptionList);
+    }
+    */
 
 }
