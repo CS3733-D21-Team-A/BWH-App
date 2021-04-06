a459ea662eb83c04b5ec9f5f2d1d874f7f36ddc7
jhiggs15 <jfhiggins98@gmail.com>
2021-04-06 15:47:25 -0400

    Completed Sanitation Service Request

--- src/main/java/edu/wpi/aquamarine_axolotls/views/Sanitation.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/Sanitation.java
@@ -1,12 +1,27 @@
 package edu.wpi.aquamarine_axolotls.views;
 
 import edu.wpi.aquamarine_axolotls.Aapp;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
 
 public class Sanitation extends Service_Request {
+
+    ObservableList<String> service_request_list = FXCollections.observableArrayList("Patient Vacancy", "Trash", "Spill");
+
+    @FXML
+    private ComboBox service_requests;
+
+
+    @FXML
+    public void initialize(){
+        service_requests.setItems(service_request_list);
+    }
 
 }
