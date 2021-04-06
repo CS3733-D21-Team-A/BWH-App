b0ce828e27a7bbe0579b2d22c19ce01d1ee99beb
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-04-05 23:30:12 -0400

    Various fixes, javadoc

--- src/main/java/edu/wpi/aquamarine_axolotls/DatabaseService.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/DatabaseService.java
@@ -8,9 +8,12 @@ import java.util.Scanner;
 public class DatabaseService {
 	private Connection connection;
 
-	private class NodeTable {
+	private class NodeTable { //TODO: Make Table interface?
 		NodeTable() {}
 
+		/**
+		 * Display list of nodes along with their attributes
+		 */
 		void nodeInformation() {
 			try {
 				Statement smnt = connection.createStatement();
@@ -45,38 +48,40 @@ public class DatabaseService {
 
 		/**
 		 * The user enters the ID of the node and is then prompted for the values of x and y coordinates
-		 * @param
-		 * @throws SQLException
 		 */
-		void updateNodeCoords() throws SQLException {
+		void updateNodeCoords() {
 			Scanner sc = new Scanner(System.in);
 
-			System.out.println("Enter nodeID");
+			System.out.print("Enter nodeID: ");
 			String nodeID = sc.next();
-			System.out.println("Enter the new X coordinate");
-			String newXC = sc.next();
-			System.out.println("Enter the new Y coordinate");
-			String newYX = sc.next();
+			System.out.print("Enter the new X coordinate: "); //TODO: check node ID before asking for values
+			int newXC = Integer.parseInt(sc.next());
+			System.out.print("Enter the new Y coordinate: ");
+			int newYC = Integer.parseInt(sc.next());
 			try {
-				Statement smnt = connection.createStatement();
-				int updatedXCoord = smnt.executeUpdate("UPDATE Nodes SET XCOORD = '" + newXC + "' WHERE NodeID = '" + nodeID + "'");
-				int updatedYCoord = smnt.executeUpdate("UPDATE Nodes SET YCOORD = '" + newYX + "' WHERE NodeID = '" + nodeID + "'");
-				if (updatedXCoord == 0) System.out.println("Error: Invalid node ID. Exiting...");
+				PreparedStatement ps = connection.prepareStatement("UPDATE Nodes SET xcoord=?, ycoord=? WHERE NodeID=?");
+				ps.setInt(1,newXC);
+				ps.setInt(2,newYC);
+				ps.setString(3,nodeID);
+				int updatedRows = ps.executeUpdate();
+				if (updatedRows == 0) System.out.println("Error: Invalid node ID. Exiting...");
 
-
-				smnt.close();
+				ps.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 
 		}
 
+		/**
+		 * Update node location long name
+		 */
 		void updateNodeLocationName() {
 			Scanner sc = new Scanner(System.in);
 
 			System.out.print("Enter node ID: ");
 			String nodeID = sc.next();
-			System.out.print("Enter the new long name for this location: ");
+			System.out.print("Enter the new long name for this location: "); //TODO: check node ID before asking for values
 			String newName = sc.next();
 
 			try {
@@ -95,32 +100,32 @@ public class DatabaseService {
 		EdgeTable() {}
 
 		/**
-		 * The program displays the list of edges along with their attributes
+		 * Display list of edges along with their attributes
 		 */
 		void edgeInformation() {
 			try {
 				Statement smnt = connection.createStatement();
-				ResultSet resultSet = smnt.executeQuery("SELECT  * FROM Edges");
-				ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-				//create display table
-				List<String[]> table = new ArrayList<>();
-				int colCount = resultSetMetaData.getColumnCount();
-				//add column label row
-				table.add(new String[colCount]);
-				//add column labels to first row
-				for(int i =0; i<colCount; i++){
-						table.get(0)[i] = resultSetMetaData.getColumnName(i+1);
+				ResultSet rset = smnt.executeQuery("SELECT * FROM Edges");
+				ResultSetMetaData rsmd = rset.getMetaData();
+
+				List<String[]> table = new ArrayList<>(); //create table to display
+				int colCount = rsmd.getColumnCount();
+
+				table.add(new String[colCount]); //add column label row
+				for (int i = 0; i < colCount; i++) {
+					table.get(0)[i] = rsmd.getColumnName(i+1); //add column labels to first row
 				}
-				//for adding row vals
-				for(int row = 0; resultSet.next(); row++){
+
+				for (int row = 1; rset.next(); row++) {
 					table.add(new String[colCount]);
-						for(int i = 0; i<colCount; i++){
-							table.get(row)[i] = resultSet.getString(resultSetMetaData.getColumnName(i+1));
-						}
+					for (int i = 0; i < colCount; i++) {
+						table.get(row)[i] = rset.getString(rsmd.getColumnName(i+1)); //add row values
+					}
 				}
+
 				//printing information
 				for(final Object[] row : table) {
-					System.out.format("%10s%10s%10s%10s%15s%10s%45s%20%n", row);
+					System.out.format("%25s%15s%15s%n", row);
 				}
 			}
 			catch (SQLException e) {
@@ -150,6 +155,10 @@ public class DatabaseService {
 
 		nodeTable = new NodeTable();
 		edgeTable = new EdgeTable();
+
+		nodeTable.nodeInformation();
+		nodeTable.updateNodeCoords();
+		nodeTable.nodeInformation();
 
 		if (arg == -1) System.out.println(
 			"1 - Node Information\n"
