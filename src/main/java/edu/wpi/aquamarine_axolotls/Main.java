72f486e9c84ad7eb4285c7f13ddfccde05425a7b
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-04-06 11:20:49 -0400

    CLI Argument interpretation / reorg

--- src/main/java/edu/wpi/aquamarine_axolotls/Main.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/Main.java
@@ -1,19 +1,10 @@
 package edu.wpi.aquamarine_axolotls;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    //Aapp.launch(Aapp.class, args);
-    //DatabaseService DB = new DatabaseService();
-    //System.out.println(DB.getEmployeeName());
-
-    DatabaseService DB = new DatabaseService(args.length == 3 ? Integer.parseInt(args[2]) : -1);
+    Aapp.launch(Aapp.class, args);
   }
 }
