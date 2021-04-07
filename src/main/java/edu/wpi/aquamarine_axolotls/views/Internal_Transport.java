bbf5ab54d1378a3e695e59cb480e49285a74a23e
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-04-07 16:14:48 -0400

    Added internal transportation service request UI

--- src/main/java/edu/wpi/aquamarine_axolotls/views/Internal_Transport.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/Internal_Transport.java
@@ -1,4 +1,55 @@
 package edu.wpi.aquamarine_axolotls.views;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Internal_Transport extends Service_Request{
+	ObservableList<String> urgencyList = FXCollections
+			  .observableArrayList("Critical", "High", "Medium", "Low");
+
+	@FXML
+	public Button back_button;
+
+	@FXML
+	public Label text;
+
+	@FXML
+	public Label formName;
+
+	@FXML
+	public Label description;
+
+	@FXML
+	public TextField firstName;
+
+	@FXML
+	public TextField lastName;
+
+	@FXML
+	public TextField roomNumber;
+
+	@FXML
+	public TextField newRoomNum;
+
+	@FXML
+	public TextField moveReason;
+
+	@FXML
+	public ComboBox urgency;
+
+	@FXML
+	public TextField specInstruct;
+
+	@FXML
+	public Button submitButton;
+
+	@FXML
+	public void initialize(){
+		urgency.setItems(urgencyList);
+	}
 }
