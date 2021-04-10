0de53a34e9a167c82e8118518ab269ef9e106887
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-04-10 18:27:26 -0400

    Simplify edge code

--- src/main/java/edu/wpi/aquamarine_axolotls/db/DatabaseController.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/db/DatabaseController.java
@@ -1,6 +1,5 @@
 package edu.wpi.aquamarine_axolotls.db;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -70,34 +69,12 @@ public class DatabaseController {
 	// ===== EDGES =====
 
 	/**
-	 * ASSUMES valid node input
-	 * @param edgeID
-	 * @return
-	 */
-	public String edgeFlip(String edgeID) throws InvalidEdgeFormatException{
-		int i = edgeID.indexOf("_");
-		// length cannot possibly be valid, value is not found, more than one occurence of _ is found
-		if(edgeID.length() <= 1 || i == -1 || edgeID.lastIndexOf("_") != i) throw new InvalidEdgeFormatException(edgeID);
-		return edgeID.substring(i + 1, edgeID.length()) + "_" + edgeID.substring(0, i);
-
-	}
-	/**
 	 * Check if an edge exists.
 	 * @param edgeID ID of edge to check.
 	 * @return Boolean indicating presence of edge in the database.
 	 */
-	public boolean edgeExists(String edgeID) throws InvalidEdgeFormatException{
-
-		try{
-			// checks edge and flipped edge ie Node1_Node2 and Node2_Node1
-			// checks null here since edited edge may be empty
-			if(edgeID == null || ( edgeTable.getEntry(edgeID) == null && edgeTable.getEntry(edgeFlip(edgeID)) == null) )
-				return false;
-		} catch (InvalidEdgeFormatException e ){
-			throw e;
-		}
-
-		return true;
+	public boolean edgeExists(String edgeID) {
+		return edgeTable.getEntry(edgeID) != null;
 	}
 
 	/**
@@ -106,17 +83,8 @@ public class DatabaseController {
 	 */
 
 	// DONE
-	public void addEdge(Map<String,String> values) throws InvalidEdgeFormatException, EdgeAlreadyExistsException{
-
-		try{
-			// edge already exists
-			if(edgeExists(values.get("edgeID"))) throw new EdgeAlreadyExistsException(values.get("edgeID"));
-			else edgeTable.addEntry(values);
-		}
-		catch (InvalidEdgeFormatException e){
-			throw e;
-		}
-
+	public void addEdge(Map<String,String> values) {
+		edgeTable.addEntry(values);
 	}
 
 	/**
@@ -126,20 +94,8 @@ public class DatabaseController {
 	 * @return Rows in database updated.
 	 */
 
-	public int editEdge(String edgeID, Map<String,String> values) throws InvalidEdgeFormatException, EdgeDoesNotExistException, EdgeAlreadyExistsException{
-		try{
-			// edge we are trying to edit does not exists
-			if(!edgeExists(edgeID)) throw new EdgeDoesNotExistException(edgeID);
-			String new_edgeID = values.get("edgeID");
-			// edge we are trying to change to already exists
-			if(new_edgeID != null && edgeExists(new_edgeID)) throw new EdgeAlreadyExistsException(new_edgeID);
-			return edgeTable.editEntry(edgeID, values);
-		}catch(InvalidEdgeFormatException e){
-			throw e;
-
-		}
-
-
+	public int editEdge(String edgeID, Map<String,String> values) {
+		return edgeTable.editEntry(edgeID, values);
 	}
 
 	/**
@@ -147,16 +103,8 @@ public class DatabaseController {
 	 * @param edgeID ID of node to delete.
 	 * @return Rows in database updated.
 	 */
-	public int deleteEdge(String edgeID) throws InvalidEdgeFormatException, EdgeDoesNotExistException{
-
-		try{
-			// edge does not exist
-			if(!edgeExists(edgeID)) throw new EdgeDoesNotExistException(edgeID);
+	public int deleteEdge(String edgeID) {
 			return edgeTable.deleteEntry(edgeID);
-		} catch (InvalidEdgeFormatException e) {
-			throw e;
-		}
-
 	}
 
 	/**
@@ -172,14 +120,8 @@ public class DatabaseController {
 	 * @param edgeID ID representing node to look for.
 	 * @return Map representing the node to query for.
 	 */
-	public Map<String,String> getEdge(String edgeID) throws InvalidEdgeFormatException, EdgeDoesNotExistException {
-		try{
-			// edge does not exist
-			if(!edgeExists(edgeID)) throw new EdgeDoesNotExistException(edgeID);
-			return edgeTable.getEntry(edgeID);
-		}catch(InvalidEdgeFormatException e){
-			throw e;
-		}
+	public Map<String,String> getEdge(String edgeID) {
+		return edgeTable.getEntry(edgeID);
 	}
 
 	/**
@@ -188,7 +130,6 @@ public class DatabaseController {
 	 * @return List of maps of edges connected to the desired node.
 	 */
 	public List<Map<String,String>> getEdgesConnectedToNode(String nodeID) {
-		if(!nodeExists(nodeID)) return null; // TODO throw error
 		// gets all edges that has nodeID as a start node
 		List<Map<String,String>> edges = edgeTable.getEntriesByValue("startNode", nodeID);
 		// gets all edges that have the nodeID as a end node
