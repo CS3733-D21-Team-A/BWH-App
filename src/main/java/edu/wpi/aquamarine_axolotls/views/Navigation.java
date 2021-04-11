de3e09fc7d3b38cc05309717af33cec55d622442
afreglett <45523745+afreglett@users.noreply.github.com>
2021-04-11 18:20:53 -0400

    dropbox

--- src/main/java/edu/wpi/aquamarine_axolotls/views/Navigation.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/Navigation.java
@@ -3,17 +3,19 @@ package edu.wpi.aquamarine_axolotls.views;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.aquamarine_axolotls.Aapp;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
+
+import javax.print.attribute.standard.Destination;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-
-import static java.lang.Math.floor;
 
 public class Navigation {
 
@@ -23,6 +25,44 @@ public class Navigation {
     @FXML private JFXComboBox startLocation;
     @FXML private JFXComboBox destination;
     @FXML private JFXButton findPathButton;
+
+
+    ObservableList<String> options =
+            FXCollections.observableArrayList(
+                    "Parking Spot 1 45 Francis Street Lobby",
+                            "Parking Spot 45 Francis Street Lobby",
+                            "Parking Spot 3 45 Francis Street Lobby",
+                            "Parking Spot 4 45 Francis Street Lobby",
+                            "Parking Spot 5 45 Francis Street Lobby",
+                            "Parking Spot 6 45 Francis Street Lobby",
+                            "Parking Spot 7 45 Francis Street Lobby",
+                            "Parking Spot 8 45 Francis Street Lobby",
+                            "Parking Spot 9 45 Francis Street Lobby",
+                            "Parking Spot 10 45 Francis Street Lobby",
+                            "Parking Spot 11 45 Francis Street Lobby",
+                            "Parking Spot 12 45 Francis Street Lobby",
+                            "Parking Spot 13 45 Francis Street Lobby",
+                            "Parking Spot 14 45 Francis Street Lobby",
+                            "Parking Spot 15 45 Francis Street Lobby",
+                            "Parking Spot 1 80 Francis Parking",
+                            "Parking Spot 2 80 Francis Parking",
+                            "Parking Spot 3 80 Francis Parking",
+                            "Parking Spot 4 80 Francis Parking",
+                            "Parking Spot 5 80 Francis Parking",
+                            "Parking Spot 6 80 Francis Parking",
+                            "Parking Spot 7 80 Francis Parking",
+                            "Parking Spot 8 80 Francis Parking",
+                            "Parking Spot 9 80 Francis Parking",
+                            "Parking Spot 10 80 Francis Parking",
+                            "Entrance 75 Francis St",
+                            "Entrance ER 75 Francis Street");
+
+    @FXML
+    public void initialize(){
+        startLocation.setItems(options);
+        destination.setItems(options);
+    }
+
 
     @FXML //need to navigate to home
     public void return_Home(ActionEvent actionEvent) {
@@ -78,8 +118,8 @@ public class Navigation {
     @FXML
     public void testdrawcircle(){
             Circle node = new Circle();
-            node.setCenterX(xScale(5000));
-            node.setCenterY(yScale(3400));
+            node.setCenterX(xScale(1 ));
+            node.setCenterY(yScale(1));
             node.setRadius(5);
             anchor.getChildren().add(node);
         }
