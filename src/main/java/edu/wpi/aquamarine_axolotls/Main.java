552322e7c20ee54e94b33d584d37e8fc4c92e0ee
billymcross <billy.m.cross@gmail.com>
2021-04-11 18:29:38 -0400

    Merged DB changes into this branch

--- src/main/java/edu/wpi/aquamarine_axolotls/Main.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/Main.java
@@ -1,5 +1,6 @@
 package edu.wpi.aquamarine_axolotls;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import edu.wpi.aquamarine_axolotls.db.DatabaseController;
@@ -11,33 +12,20 @@ public class Main {
     //Aapp.launch(Aapp.class, args);
     Adb.main(args);
 
-    //Declare file paths for node and edge CSVs
-    String nodeFileName = "src/main/resources/edu/wpi/aquamarine_axolotls/MapAnodes.csv";
-    String edgeFileName = "src/main/resources/edu/wpi/aquamarine_axolotls/MapAedges.csv";
-
-    //Setup node and edge lists from the CSV files
-    //List<Node> nodes = new CsvToBeanBuilder(new FileReader(nodeFileName)).withType(Node.class).build().parse();
-    //List<Edge> edges = new CsvToBeanBuilder(new FileReader(edgeFileName)).withType(Edge.class).build().parse();
-
     DatabaseController dbControl = new DatabaseController();
 
-    List<Map<String, String>> nodeMap = dbControl.getNodes();
-    List<Map<String, String>> edgeMap = dbControl.getEdges();
-
-    //System.out.println(nodes);
-
-    //Initialize the reader to get the data
-    //Edge newEdge = new Edge();
-    //Node newNode = new Node();
+    try {
+      List<Map<String, String>> nodeMap = dbControl.getNodes();
+      List<Map<String, String>> edgeMap = dbControl.getEdges();
+    }
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
 
     //Initialize the search controller
     SearchAlgorithm newSearch = new SearchAlgorithm();
 
     //List<Node> connectedNode = Node.getConnected(newNode.getNode("CDEPT003L1",nodes), edges, nodes);
-
-    //Get the start and end nodes from the list of nodes
-    //Node start = newNode.getNode("aPARK016GG",nodes);
-    //Node goal = newNode.getNode("aPARK007GG",nodes);
 
     String start = "aPARK016GG";
     String goal = "aPARK007GG";
