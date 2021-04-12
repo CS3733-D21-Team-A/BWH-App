efe229a856fc7a22b647fa16459215b7f504e204
evelyntrvn <evelyntran235@gmail.com>
2021-04-12 12:33:23 -0400

    added edge_Editing.fxml and small updates to EdgeEditing.java and NodeEditing.java

---
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/NodeEditing.java
@@ -0,0 +1,10 @@
+package edu.wpi.aquamarine_axolotls.views;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
+public class NodeEditing {
+    @FXML private JFXButton homeButton;
+    @FXML private JFXButton helpButton;
+
+}
