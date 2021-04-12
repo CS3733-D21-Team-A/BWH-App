fc7d46662caecf39c3de86f466d2d18fd9222eef
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-04-12 19:39:15 -0400

    Remove unneeded code

--- src/main/java/edu/wpi/aquamarine_axolotls/Main.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/Main.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     //Aapp.launch(Aapp.class, args);
-    Adb.main(args);
   }
 }
