28bf482615da695779a25c90a3b9a7f82273331e
jhiggs15 <jfhiggins98@gmail.com>
2021-04-11 21:49:59 -0400

    Placed a few points, updated img scaling, changed excel sheets

--- src/main/java/edu/wpi/aquamarine_axolotls/views/Navigation.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/Navigation.java
@@ -3,6 +3,7 @@ package edu.wpi.aquamarine_axolotls.views;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.aquamarine_axolotls.Aapp;
+import edu.wpi.aquamarine_axolotls.db.DatabaseController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,6 +15,7 @@ import javafx.scene.shape.Circle;
 
 import javax.print.attribute.standard.Destination;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -25,10 +27,7 @@ public class Navigation {
     @FXML private JFXComboBox startLocation;
     @FXML private JFXComboBox destination;
     @FXML private JFXButton findPathButton;
-
-
-    ObservableList<String> options =
-            FXCollections.observableArrayList(
+    ObservableList<String> options = FXCollections.observableArrayList(
                     "Parking Spot 1 45 Francis Street Lobby",
                             "Parking Spot 45 Francis Street Lobby",
                             "Parking Spot 3 45 Francis Street Lobby",
@@ -56,13 +55,13 @@ public class Navigation {
                             "Parking Spot 10 80 Francis Parking",
                             "Entrance 75 Francis St",
                             "Entrance ER 75 Francis Street");
+    DatabaseController db = new DatabaseController();
 
     @FXML
     public void initialize(){
         startLocation.setItems(options);
         destination.setItems(options);
     }
-
 
     @FXML //need to navigate to home
     public void return_Home(ActionEvent actionEvent) {
@@ -92,7 +91,7 @@ public class Navigation {
 
     public Double yScale(int yCoord){
         Double yCoordDouble = new Double(yCoord);
-        Double imgWidth = 327.0;
+        Double imgWidth = 298.0;
         Double proportion = imgWidth/3400;
 
         Double newYCoord = yCoordDouble*proportion; //may need to add margins depending on how it's placed on
@@ -100,29 +99,32 @@ public class Navigation {
         return newYCoord;
     }
 
-    public void initiateNodes(List<Map<String,String>> nodes){
-        for (int i = 0; i < nodes.size(); i++){
-            Map<String, String> currentNode = nodes.get(i);
-            Double scaledX= xScale(Integer.parseInt(currentNode.get("xcoord")));
-            Double scaledY= yScale(Integer.parseInt(currentNode.get("ycoord")));
+    public void initiateNodes(){
+        try{
+            Map<String, String> node = db.getNode("aPARK001GG"); // only displays a node and top left and bottom right
+            Circle circ = new Circle();
+            circ.setCenterX(xScale(Integer.parseInt(node.get("XCOORD")) ));
+            circ.setCenterY(yScale(Integer.parseInt(node.get("YCOORD")) ));
+            circ.setRadius(1);
+            anchor.getChildren().add(circ);
 
-            Circle node = new Circle();
-            node.setCenterX(5000);
-            node.setCenterY(3400);
-            node.setRadius(4);
-            anchor.getChildren().add(node);
+            circ = new Circle();
+            circ.setCenterX(0);
+            circ.setCenterY(0);
+            circ.setRadius(4);
+            anchor.getChildren().add(circ);
 
+            circ = new Circle();
+            circ.setCenterX(xScale(5000));
+            circ.setCenterY(yScale(3400));
+            circ.setRadius(1);
+            anchor.getChildren().add(circ);
+        } catch(SQLException e){
+            e.printStackTrace();
         }
         return;
     }
-    @FXML
-    public void testdrawcircle(){
-            Circle node = new Circle();
-            node.setCenterX(xScale(1 ));
-            node.setCenterY(yScale(1));
-            node.setRadius(5);
-            anchor.getChildren().add(node);
-        }
+
 
 
 }
