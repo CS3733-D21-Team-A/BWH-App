cfc7e2d33e792b850465e37dba63580752640712
sirrus98 <46726516+sirrus98@users.noreply.github.com>
2021-04-13 21:55:19 -0400

    fix IO exception error

--- src/main/java/edu/wpi/aquamarine_axolotls/Main.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/Main.java
@@ -2,7 +2,7 @@ package edu.wpi.aquamarine_axolotls;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     //Aapp.launch(Aapp.class, args);
   }
 }
