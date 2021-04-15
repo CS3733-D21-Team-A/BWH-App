d18ec42f5e079d9cce0404519b019716fdd843fa
jhiggs15 <jfhiggins98@gmail.com>
2021-04-14 22:32:42 -0400

    Finished

--- src/main/java/edu/wpi/aquamarine_axolotls/views/Admin_Main_Page.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/Admin_Main_Page.java
@@ -1,19 +1,40 @@
 package edu.wpi.aquamarine_axolotls.views;
 
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
 import edu.wpi.aquamarine_axolotls.Aapp;
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+//import javafx.application.Platform.exit;
+//import javafx.application.Platform.exit;
 import java.io.IOException;
-import javafx.event.EventHandler;
-import javafx.stage.Popup;
 
 public class Admin_Main_Page {
     @FXML
-    private Button service_req;
+    private JFXButton sign_in_button;
+
+    @FXML
+    private JFXButton map_button;
+
+    @FXML
+    private JFXButton service_req;
+
+    @FXML
+    private JFXButton node_editing_button;
+
+    @FXML
+    private JFXButton edge_editing;
+
+    @FXML
+    private StackPane stackPane;
 
     @FXML
     public void Sign_In_Button(ActionEvent actionEvent) {
@@ -27,22 +48,50 @@ public class Admin_Main_Page {
     }
 
     @FXML
-    public void service_comp(ActionEvent actionEvent) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/aquamarine_axolotls/fxml/Default_Service_Page.fxml"));
-            Aapp.getPrimaryStage().getScene().setRoot(root);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-    @FXML
-    public void Map_Page(ActionEvent actionEvent) {
+    public void Map_Button(ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/aquamarine_axolotls/fxml/Navigation.fxml"));
             Aapp.getPrimaryStage().getScene().setRoot(root);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
+    }
+
+    @FXML
+    public void service_request_page(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/aquamarine_axolotls/fxml/Default_Service_Page.fxml"));
+            Aapp.getPrimaryStage().getScene().setRoot(root);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+
+    public void stop(javafx.event.ActionEvent event){
+        JFXDialogLayout content = new JFXDialogLayout();
+        JFXDialog help = new JFXDialog(stackPane, content, JFXDialog.DialogTransition.BOTTOM);
+        content.setHeading(new Text("Shut Down Application"));
+        content.setBody(new Text("Clicking the Shut Down button will close the application."));
+        JFXButton shutDownButton = new JFXButton("Shut Down");
+        JFXButton cancel_button = new JFXButton( "Cancel");
+        shutDownButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                help.close();
+                Platform.exit();
+            }
+        });
+        cancel_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                help.close();
+            }
+        });
+        content.setActions(cancel_button, shutDownButton);
+        help.show();
     }
 
     @FXML
