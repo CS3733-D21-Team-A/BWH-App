e9f5eb8aacd329ef335fb16b5e216b76dd6aca70
Galputer <eakelley2001@gmail.com>
2021-04-18 14:04:28 -0400

    Made stubs for dbcontroller tests

--- src/test/java/edu/wpi/aquamarine_axolotls/db/DatabaseControllerTest3.java
+++ src/test/java/edu/wpi/aquamarine_axolotls/db/DatabaseControllerTest3.java
@@ -1,4 +1,25 @@
 package edu.wpi.aquamarine_axolotls.db;
 
 public class DatabaseControllerTest3 {
+    // Tests for iteration 1
+    // ***Be sure to break up tests by method***
+    //  Test every functionally different case that differs from each other
+    //  (testing functionality)
+    // don't hard code, you need to write the tests including every case
+    // test for functionality not for place holders
+
+    // N'yoma testing CSV Handler
+
+
+    // CJ and Sean testing service requests
+
+    // Chris and Zhongchuan testing attributes
+
+    // Emily testing getNodes and getEdges
+    public void getNodesTesting(){
+
+    }
+    public void getEdgesTesting(){
+
+    }
 }
