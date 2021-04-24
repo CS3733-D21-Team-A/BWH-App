a4a31f8a37a44814e46898f459cac4633bfd08d3
sirrus98 <46726516+sirrus98@users.noreply.github.com>
2021-04-24 14:46:56 -0400

    implemented BFS not tested

--- src/main/java/edu/wpi/aquamarine_axolotls/pathplanning/AbsAlgorithmMethod.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/pathplanning/AbsAlgorithmMethod.java
@@ -1,7 +1,9 @@
 package edu.wpi.aquamarine_axolotls.pathplanning;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public abstract class AbsAlgorithmMethod implements ISearchAlgorithmStrategy{
 
@@ -111,6 +113,31 @@ public abstract class AbsAlgorithmMethod implements ISearchAlgorithmStrategy{
         double dist = Math.sqrt(xsqre+ysqre);
 
         return dist;
+    }
+
+    /**
+     * Builds a list of nodes that show the path from the start to the end
+     * Meant to be run AFTER the full search algorithm is complete
+     * @param cameFrom The list of nodes listed alongside which nodes came before them in the path
+     * @param goal The goal node
+     * @return A list of all the nodes used to make the path
+     */
+    protected List<Node> buildPath(final Map<Node, Node> cameFrom, final Node goal) {
+        //Initialize the path
+        LinkedList<Node> path = new LinkedList<>();
+        //Current node is the goal
+        Node next = goal;
+        //Trace the path backwards from the goal
+        //Each loop, we'll get the next node back from the current one on the path
+        while (cameFrom.get(next) != null) {
+            //Add that next node
+            path.push(next);
+            //Move back on the path another step
+            next = cameFrom.get(next);
+        }
+        //Add the last node
+        path.push(next);
+        return path;
     }
 
     /**
