ed39c768d8ecb0978fbcde24b6524e885a0e06dc
jhiggs15 <56650682+jhiggs15@users.noreply.github.com>
2021-04-26 21:05:27 -0400

    Updated Employee Requests to pull available users from the database

--- src/main/java/edu/wpi/aquamarine_axolotls/views/EmployeeRequests.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/EmployeeRequests.java
@@ -5,6 +5,7 @@ import com.jfoenix.controls.JFXComboBox;
 import edu.wpi.aquamarine_axolotls.db.DatabaseController;
 import edu.wpi.aquamarine_axolotls.db.*;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -13,66 +14,62 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class EmployeeRequests extends SPage{
 
-    @FXML
-    private TableView<EmployeeRequest> srTable;
-
-    @FXML
-    TableColumn<EmployeeRequest, String> assignedColumn;
-
-    @FXML
-    TableColumn<EmployeeRequest, String> assigneeColumn;
-
-    @FXML
-    TableColumn<EmployeeRequest, String> statusColumn;
-
-    @FXML
-    TableColumn<EmployeeRequest, String> serviceRequestColumn;
-
-    @FXML
-    TableColumn<EmployeeRequest, String> locationColumn;
-
-    @FXML
-    JFXButton assignB;
-
-    @FXML
-    JFXButton changeStatusB;
-
-    @FXML
-    JFXButton cancelB;
-
-    @FXML
-    JFXComboBox assignD;
-
-    @FXML
-    JFXComboBox statusD;
-
-    @FXML
-    JFXComboBox filterD;
+    @FXML private TableView<EmployeeRequest> srTable;
+    @FXML private TableColumn<EmployeeRequest, String> assignedColumn;
+    @FXML private TableColumn<EmployeeRequest, String> assigneeColumn;
+    @FXML private TableColumn<EmployeeRequest, String> statusColumn;
+    @FXML private TableColumn<EmployeeRequest, String> serviceRequestColumn;
+    @FXML private TableColumn<EmployeeRequest, String> locationColumn;
+    @FXML private JFXButton assignB;
+    @FXML private JFXButton changeStatusB;
+    @FXML private JFXComboBox assignD;
+    @FXML JFXComboBox statusD;
+    @FXML JFXComboBox filterD;
     
     DatabaseController db;
 
 
     @FXML
     public void initialize() { // creds : http://tutorials.jenkov.com/javafx/tableview.html
-        assignD.setItems(FXCollections
-                .observableArrayList("Sai", "Samantha", "Imani"));
+        try {
+            db = new DatabaseController();
+            Map<String, String> user1 = new HashMap<>();
+            user1.put("USERNAME", "poophead");
+            user1.put("FIRSTNAME","poo");
+            user1.put("LASTNAME","head");
+            user1.put("EMAIL","phead@gmail.com");
+            user1.put("USERTYPE", "Employee");
+            user1.put("PASSWORD", "123");
 
-        statusD.setItems(FXCollections
-                .observableArrayList(DatabaseUtil.STATUS_NAMES.get(STATUS.IN_PROGRESS),
-                   DatabaseUtil.STATUS_NAMES.get(STATUS.DONE),
-                   DatabaseUtil.STATUS_NAMES.get(STATUS.CANCELED)));
-        assignedColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("assigned"));
-        assigneeColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("assignee"));
-        statusColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("status"));
-        serviceRequestColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("serviceRequest"));
-        locationColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("location"));
 
-        refresh();
+            db.addUser(user1);
+            ObservableList<String> employeeNames = FXCollections.observableArrayList();
+            List<Map<String, String>> users = db.getUsers();
+            for(Map<String, String> user : users){
+                if(user.get("USERTYPE").equals("Employee")) employeeNames.add(user.get("FIRSTNAME") + user.get("LASTNAME"));
+            }
+            assignD.setItems(employeeNames);
+            statusD.setItems(FXCollections
+                    .observableArrayList(DatabaseUtil.STATUS_NAMES.get(STATUS.IN_PROGRESS),
+                            DatabaseUtil.STATUS_NAMES.get(STATUS.DONE),
+                            DatabaseUtil.STATUS_NAMES.get(STATUS.CANCELED)));
+            assignedColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("assigned"));
+            assigneeColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("assignee"));
+            statusColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("status"));
+            serviceRequestColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("serviceRequest"));
+            locationColumn.setCellValueFactory(new PropertyValueFactory<EmployeeRequest, String>("location"));
+
+            refresh();
+        } catch (SQLException | IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void goHome(ActionEvent actionEvent) {
