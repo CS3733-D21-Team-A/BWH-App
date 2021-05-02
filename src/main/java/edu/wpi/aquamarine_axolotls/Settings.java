077d9181635a87948eb107b4605118ff9cc34a2b
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-05-02 16:55:13 -0400

    minor edits

--- src/main/java/edu/wpi/aquamarine_axolotls/Settings.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/Settings.java
@@ -2,10 +2,10 @@ package edu.wpi.aquamarine_axolotls;
 
 import java.util.prefs.*;
 
-public class Settings { //functionally static
+public class Settings {
 	private Settings(){} //NO INSTANTIATION >:(
 
-	public final static Preferences prefs = Preferences.userNodeForPackage(Aapp.class);
+	public final static Preferences prefs = Preferences.userNodeForPackage(Settings.class);
 
 	final public static String API_KEY = "Google Maps API Key";
 }
