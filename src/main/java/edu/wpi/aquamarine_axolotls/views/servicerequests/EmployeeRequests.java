5218a3d4ac2472a093c8b84f177cff1af7a3e42e
evelyntrvn <evelyntran235@gmail.com>
2021-05-03 12:28:48 -0400

    added COVID Survey table; have not tested

--- src/main/java/edu/wpi/aquamarine_axolotls/views/servicerequests/EmployeeRequests.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/servicerequests/EmployeeRequests.java
@@ -28,6 +28,21 @@ public class EmployeeRequests extends GenericPage { //TODO: please change the na
     @FXML private TableColumn<Request, String> statusColumn;
     @FXML private TableColumn<Request, String> serviceRequestColumn;
     @FXML private TableColumn<Request, String> locationColumn;
+
+    @FXML private TableView<CovidSurvey> covidSurveyTable;
+    @FXML private TableColumn<CovidSurvey, String> usernameColumn;
+    @FXML private TableColumn<CovidSurvey, String> feverColumn;
+    @FXML private TableColumn<CovidSurvey, String> coughColumn;
+    @FXML private TableColumn<CovidSurvey, String> shortnessOfBreathColumn;
+    @FXML private TableColumn<CovidSurvey, String> muscleAchesColumn;
+    @FXML private TableColumn<CovidSurvey, String> tiredColumn;
+    @FXML private TableColumn<CovidSurvey, String> nasalColumn;
+    @FXML private TableColumn<CovidSurvey, String> soreThroatColumn;
+    @FXML private TableColumn<CovidSurvey, String> nauseaDiarrheaColumn;
+    @FXML private TableColumn<CovidSurvey, String> lossOfTasteSmellColumn;
+    @FXML private TableColumn<CovidSurvey, String> chillsColumn;
+    @FXML private TableColumn<CovidSurvey, String> quarantineColumn;
+
     @FXML private JFXButton assignB;
     @FXML private JFXButton changeStatusB;
     @FXML private JFXComboBox assignD;
@@ -78,6 +93,17 @@ public class EmployeeRequests extends GenericPage { //TODO: please change the na
             statusColumn.setCellValueFactory(new PropertyValueFactory<Request, String>("status"));
             serviceRequestColumn.setCellValueFactory(new PropertyValueFactory<Request, String>("serviceRequest"));
             locationColumn.setCellValueFactory(new PropertyValueFactory<Request, String>("location"));
+
+            usernameColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("username"));
+            feverColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("fever"));
+            coughColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("cough"));
+            shortnessOfBreathColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("shortnessOfBreath"));
+            muscleAchesColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("muscleAches"));
+            tiredColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("tired"));
+            nasalColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("nasalCongestion"));
+            lossOfTasteSmellColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("lossOfTasteSmell"));
+            chillsColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("chills"));
+            quarantineColumn.setCellValueFactory(new PropertyValueFactory<CovidSurvey, String>("quarantine"));
 
             refresh();
         } catch (SQLException | IOException e) {
@@ -195,6 +221,124 @@ public class EmployeeRequests extends GenericPage { //TODO: please change the na
             this.serviceRequest = sr.get("REQUESTTYPE");
             this.location = sr.get("LOCATIONID");
             this.requestID = sr.get("REQUESTID");
+        }
+    }
+
+    public class CovidSurvey { //This needs to be public for things to work :(
+        private String username;
+        private String fever;
+        private String cough;
+        private String shortnessOfBreath;
+        private String muscleAches;
+        private String tired;
+        private String nasalCongestion;
+        private String soreThroat;
+        private String nauseaDiarrhea;
+        private String lossOfTasteSmell;
+        private String chills;
+        private String quarantine;
+
+        public CovidSurvey(Map<String, String> survey) {
+            this.username = survey.get("USERNAME");
+            this.quarantine = survey.get("AREQUAR");
+            this.fever = survey.get("HASFEVER");
+            this.cough = survey.get("HASCOUGH");
+            this.shortnessOfBreath = survey.get("SHORTBREATH");
+            this.muscleAches = survey.get("MUSCLEACHES");
+            this.tired = survey.get("MORETIRED");
+            this.nasalCongestion = survey.get("NASALCONGEST");
+            this.soreThroat = survey.get("SORETHROAT");
+            this.nauseaDiarrhea = survey.get("NAUSEADIARRHEA");
+            this.lossOfTasteSmell = survey.get("LOSSTASTESMELL");
+            this.chills = survey.get("NEWCHILLS");
+        }
+
+        public String isFever() {
+            return fever;
+        }
+
+        public void setFever(String fever) {
+            this.fever = fever;
+        }
+
+        public String isCough() {
+            return cough;
+        }
+
+        public void setCough(String cough) {
+            this.cough = cough;
+        }
+
+        public String isShortnessOfBreath() {
+            return shortnessOfBreath;
+        }
+
+        public void setShortnessOfBreath(String shortnessOfBreath) {
+            this.shortnessOfBreath = shortnessOfBreath;
+        }
+
+        public String isMuscleAches() {
+            return muscleAches;
+        }
+
+        public void setMuscleAches(String muscleAches) {
+            this.muscleAches = muscleAches;
+        }
+
+        public String isTired() {
+            return tired;
+        }
+
+        public void setTired(String tired) {
+            this.tired = tired;
+        }
+
+        public String isNasalCongestion() {
+            return nasalCongestion;
+        }
+
+        public void setNasalCongestion(String nasalCongestion) {
+            this.nasalCongestion = nasalCongestion;
+        }
+
+        public String isSoreThroat() {
+            return soreThroat;
+        }
+
+        public void setSoreThroat(String soreThroat) {
+            this.soreThroat = soreThroat;
+        }
+
+        public String isNauseaDiarrhea() {
+            return nauseaDiarrhea;
+        }
+
+        public void setNauseaDiarrhea(String nauseaDiarrhea) {
+            this.nauseaDiarrhea = nauseaDiarrhea;
+        }
+
+        public String isLossOfTasteSmell() {
+            return lossOfTasteSmell;
+        }
+
+        public void setLossOfTasteSmell(String lossOfTasteSmell) {
+            this.lossOfTasteSmell = lossOfTasteSmell;
+        }
+
+        public String isChills() {
+            return chills;
+        }
+
+        public void setChills(String chills) {
+            this.chills = chills;
+        }
+
+        public String isQuarantine() {
+            return quarantine;
+        }
+
+        public void setQuarantine(String quarantine) {
+            this.quarantine = quarantine;
         }
     }
 
