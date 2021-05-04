b900ee508933bc87c81aeaf89a5bed88c3cdc54e
billymcross <billy.m.cross@gmail.com>
2021-05-04 12:07:38 -0400

    Intermediate destinations now appear on the correct floors

--- src/main/java/edu/wpi/aquamarine_axolotls/views/mapping/Navigation.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/views/mapping/Navigation.java
@@ -202,9 +202,10 @@ public class Navigation extends GenericMap {
         if(activePath == 1) {
             drawPath(floor);
             for (Map<String, String> intermediatePointToDraw : intermediatePoints) {
-                drawSingleNodeHighLight(intermediatePointToDraw, Color.ORANGE);
+                if (intermediatePointToDraw.get("FLOOR").equals(FLOOR)) drawSingleNodeHighLight(intermediatePointToDraw, Color.ORANGE);
             }
-            drawSingleNodeHighLight(intermediatePoints.get(intermediatePoints.size()-1),Color.MAGENTA);
+            if (intermediatePoints.get(intermediatePoints.size() - 1).get("FLOOR").equals(FLOOR))
+                drawSingleNodeHighLight(intermediatePoints.get(intermediatePoints.size()-1),Color.MAGENTA);
         }
         else drawNodes(Color.BLUE);
     }
