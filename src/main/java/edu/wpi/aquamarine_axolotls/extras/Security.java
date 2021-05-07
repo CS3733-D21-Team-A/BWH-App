6de450fa2da64c6fcb0f83c68969ea9b7cbcb9af
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-05-07 13:37:49 -0400

    Finish Security methods

--- src/main/java/edu/wpi/aquamarine_axolotls/extras/Security.java
+++ src/main/java/edu/wpi/aquamarine_axolotls/extras/Security.java
@@ -3,38 +3,78 @@ package edu.wpi.aquamarine_axolotls.extras;
 import dev.samstevens.totp.code.CodeVerifier;
 import dev.samstevens.totp.code.DefaultCodeGenerator;
 import dev.samstevens.totp.code.DefaultCodeVerifier;
+import dev.samstevens.totp.exceptions.QrGenerationException;
+import dev.samstevens.totp.qr.QrData;
+import dev.samstevens.totp.qr.ZxingPngQrGenerator;
 import dev.samstevens.totp.secret.DefaultSecretGenerator;
 import dev.samstevens.totp.secret.SecretGenerator;
+import dev.samstevens.totp.time.NtpTimeProvider;
 import dev.samstevens.totp.time.SystemTimeProvider;
+import dev.samstevens.totp.time.TimeProvider;
 import edu.wpi.aquamarine_axolotls.db.DatabaseController;
+import javafx.util.Pair;
 
-import java.io.IOException;
+import java.net.UnknownHostException;
 import java.sql.SQLException;
+import java.util.Collections;
 
 public class Security {
 	private Security(){} //NO INSTANTIATION >:(
 
+	final static private DatabaseController dbController = DatabaseController.getInstance();
 	private final static SecretGenerator secretGenerator = new DefaultSecretGenerator();
-	private final static CodeVerifier codeVerifier = new DefaultCodeVerifier(new DefaultCodeGenerator(), new SystemTimeProvider()); //TODO: NTP time provider instead?
+	private final static CodeVerifier codeVerifier;
 
-	// dbController can't be final because the database connection can be changed, so we need to be able to update it
-	//private static DatabaseController dbController;
+	static {
+		TimeProvider tp;
 
-	/*static {
 		try {
-			dbController = DatabaseController.getInstance();
-		} catch (SQLException | IOException e) {
-			e.printStackTrace();
+			tp = new NtpTimeProvider("pool.ntp.org");
+		} catch (UnknownHostException e) {
+			System.out.println("Unable to establish connection with pool.ntp.org, reverting to system time for 2FA");
+			tp = new SystemTimeProvider();
 		}
-	}*/
-
-
-	public static String generateSecret() {
-		return secretGenerator.generate();
+		codeVerifier = new DefaultCodeVerifier(new DefaultCodeGenerator(), tp);
 	}
 
+
+	/**
+	 * Enables TOTP 2FA for the provided user
+	 * @return User's TOTP secret and a QR code for it
+	 * @param username Username of user to enable TOTP 2FA for
+	 * @throws SQLException Something went wrong.
+	 */
+	public static Pair<String,byte[]> enableTOTP(String username) throws SQLException, QrGenerationException {
+		String secret = secretGenerator.generate();
+		dbController.editUser(username, Collections.singletonMap("TOTPSECRET",secret));
+		return new Pair<>(secret, makeQrCode(username,secret));
+	}
+
+	/**
+	 * TOTP 2FA verification
+	 * @param username username of user to verify
+	 * @param passcode passcode to attempt verification with
+	 * @return if verification was successful
+	 * @throws SQLException Something went wrong.
+	 */
 	public static boolean verifyTOTP(String username, String passcode) throws SQLException {
-		//codeVerifier.isValidCode(dbController.getUserByUsername(username).get("TOTPSECRET"), passcode);
-		return false;
+		return codeVerifier.isValidCode(dbController.getUserByUsername(username).get("TOTPSECRET"), passcode);
+	}
+
+	/**
+	 * Generate a TOTP QR code based on the provided username and secret
+	 * @param username username of user the TOTP secret is for
+	 * @param secret TOTP secret
+	 * @return QR code as byte array
+	 * @throws QrGenerationException Something went wrong.
+	 */
+	private static byte[] makeQrCode(String username, String secret) throws QrGenerationException {
+		QrData qrData = new QrData.Builder()
+			.label(username)
+			.secret(secret)
+			.issuer("BWH")
+			.build();
+
+		return new ZxingPngQrGenerator().generate(qrData);
 	}
 }
